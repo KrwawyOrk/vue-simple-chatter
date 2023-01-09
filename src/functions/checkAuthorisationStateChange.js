aadf2eb471ebcd300de4ab05dbf766f1893f25f1
KrwawyOrk <rajzendre@gmail.com>
2023-01-09 15:18:25 +0100

    fixed bug when user closes browser tab

--- src/functions/checkAuthorisationStateChange.js
+++ src/functions/checkAuthorisationStateChange.js
@@ -1,22 +1,16 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import userStateAndFunctions from "./userStateAndFunctions.js";
-import loginFunctions from "./loginFunctions.js";
-import useUsersOnlineListStateAndFunctions from "./useUsersOnlineListStateAndFunctions.js";
+import listenToUserCloseBrowserTab from "./listenToUserCloseBrowserTab.js";
 
 function checkAuthorisationStateChange() {
   const { setUser, removeUser } = userStateAndFunctions;
-  const { signOutUserFromFirebase } = loginFunctions;
-  const { setUserStatusOnline } = useUsersOnlineListStateAndFunctions();
 
   onAuthStateChanged(getAuth(), (user) => {
     user ? setUser(user) : removeUser();
   });
 
-  window.addEventListener("beforeunload", (event) => {
-    setUserStatusOnline(false);
-    signOutUserFromFirebase();
-  });
+  listenToUserCloseBrowserTab();
 }
 
 export default checkAuthorisationStateChange;
--- src/functions/listenToUserCloseBrowserTab.js
+++ src/functions/listenToUserCloseBrowserTab.js
@@ -1,14 +1,10 @@
-import userStateAndFunctions from "./userStateAndFunctions.js";
-import useUsersOnlineListStateAndFunctions from "./useUsersOnlineListStateAndFunctions";
 import loginFunctions from "./loginFunctions";
 
-const { removeUser } = userStateAndFunctions;
-const { setUserStatusOnline } = useUsersOnlineListStateAndFunctions();
 const { signOutUserFromFirebase } = loginFunctions;
 
 function listenToUserCloseBrowserTab() {
   window.addEventListener("beforeunload", (event) => {
-    removeUser();
+    signOutUserFromFirebase();
   });
 }
 
